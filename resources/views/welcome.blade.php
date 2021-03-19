93512524e8041ef14f06d2a008c2f4004f31658c
Tubagus Gusti Fauzy <fravens26@gmail.com>
2021-03-19 13:22:39 +0700

    ubha nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -194,7 +194,7 @@
                     <div class="col-xl-9 col-lg-8">
                         <div class="footer-copy-right">
                             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script><i class="fa fa-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Riksa Paradila Pasa</a>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script><i class="fa fa-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Ahmad Syawal</a>
   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                         </div>
                     </div>
