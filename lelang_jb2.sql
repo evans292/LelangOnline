66bef25ac158221c337df145be5cf6ed53ca3494
Tubagus Gusti Fauzy <fravens26@gmail.com>
2021-03-19 11:08:48 +0700

    tmbah file database

---
+++ lelang_jb2.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2021 at 05:05 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lelang_jb2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_lelang`
+--
+
+CREATE TABLE `history_lelang` (
+  `id_history` int(11) NOT NULL,
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `penawaran_harga` int(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history_lelang`
+--
+
+INSERT INTO `history_lelang` (`id_history`, `id_lelang`, `id_barang`, `id_user`, `penawaran_harga`, `created_at`, `updated_at`) VALUES
+(3, 2, 3, 2, 600000, '2021-03-18 06:19:40', '2021-03-18 06:19:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `tgl` date NOT NULL,
+  `harga_awal` int(20) NOT NULL,
+  `deskripsi_barang` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`, `created_at`, `updated_at`) VALUES
+(3, 'Dompet Gucci', '2021-03-18', 500000, 'ini dompet mahal', '2021-03-18 06:12:41', '2021-03-18 06:12:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_lelang`
+--
+
+CREATE TABLE `tb_lelang` (
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_lelang` date NOT NULL,
+  `harga_akhir` int(20) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `status` enum('dibuka','ditutup','','') NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_lelang`
+--
+
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_lelang`, `harga_akhir`, `id_user`, `id_petugas`, `status`, `created_at`, `updated_at`) VALUES
+(2, 3, '2021-03-18', 600000, 2, 2, 'ditutup', '2021-03-18 06:16:54', '2021-03-18 06:20:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_level`
+--
+
+CREATE TABLE `tb_level` (
+  `id_level` int(11) NOT NULL,
+  `level` enum('administrator','petugas','','') NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_level`
+--
+
+INSERT INTO `tb_level` (`id_level`, `level`, `created_at`, `updated_at`) VALUES
+(1, 'petugas', NULL, NULL),
+(2, 'administrator', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_masyarakat`
+--
+
+CREATE TABLE `tb_masyarakat` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `telp` varchar(25) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_masyarakat`
+--
+
+INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `pass`, `telp`, `created_at`, `updated_at`) VALUES
+(1, 'Tubagus Gusti Fauzy', 'tubagusgf', '$2y$10$Jx9gAhgGD0lxOesVPKqLeuU9Hpa4OVBrJCByHmaAEyYu0d1QGsyoi', '085155156437', '2021-03-18 04:00:59', '2021-03-18 04:00:59'),
+(2, 'Sultan', 'sultan', '$2y$10$YZGCrkeH9pxQdJFBtWYn5OA7Sl/jht.GWcHOn4M3tdyM6KDRDnzJq', '085155156437', '2021-03-18 05:41:46', '2021-03-18 05:41:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_level` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `id_level`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin', '$2b$10$5bFzf3UWCsm7jB4Nskq.DeS0KlysHK022wI87ae78MtztZgGSANTy', 2, NULL, NULL),
+(2, 'Petugas', 'petugas', '$2y$10$0jSOUH5MuRcCMbuNwioms.fBRhfHWIq9lWk7j83T.iHVHLkQKvUDC', 1, '2021-03-18 04:41:17', '2021-03-18 04:41:17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  ADD PRIMARY KEY (`id_history`);
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD PRIMARY KEY (`id_lelang`);
+
+--
+-- Indexes for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indexes for table `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
